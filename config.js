a7bfad3b8e241023499c5a864a8b6c3390bd63cd
BAIYESIPO <148212993+BAIYESIPO@users.noreply.github.com>
2025-03-13 07:55:12 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,68 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "",
+    "CookieArray": [],
+    "WastedCookie": [],
+    "unknownModels": ["claude-3-opus","claude-3-5-sonnet-20241022","claude_3_7_sonnet","claude_3_7_sonnet_thinking"],
+    "Cookiecounter": 3,
+    "CookieIndex": 0,
+    "ProxyPassword": "",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "localtunnel": false,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "",
+    "api_rProxy": "https://glamoth.ar26710.online/v1",
+    "placeholder_token": "",
+    "placeholder_byte": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": true,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": false,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": "1000,1000,15000",
+        "xmlPlot": true,
+        "SkipRestricted": false,
+        "Artifacts": false,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
